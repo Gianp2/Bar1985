e7e06348f01728a293b2e9167cbf1ac33e6173bd
Gianluca Pasquinelli <117898176+Gianp2@users.noreply.github.com>
2025-09-10 17:36:51 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -20,11 +20,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyCKsjeGtSUEI_umn_MPMzqbES2T-NUt_Vs",
   authDomain: "bar1985-d34ea.firebaseapp.com",
   projectId: "bar1985-d34ea",
-  storageBucket: "bar1985-d34ea.firebasestorage.app",
+  storageBucket: "bar1985-d34ea.appspot.com", // CORREGIDO
   messagingSenderId: "1010372504350",
   appId: "1:1010372504350:web:5b7a4f88bb6a52d270b70f",
   measurementId: "G-1TEEFS4FL2"
 };
+
 
 // Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
