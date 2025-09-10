48e741cbb2e2bb23754726dc0131ee7e05f8c674
Gianluca Pasquinelli <117898176+Gianp2@users.noreply.github.com>
2025-09-10 17:21:03 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,32 @@
-// Firebase initialization (generated)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, serverTimestamp, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import { deleteDoc, updateDoc, doc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { 
+  getAuth, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  deleteDoc, 
+  doc, 
+  getDocs, 
+  serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKsjeGtSUEI_umn_MPMzqbES2T-NUt_Vs",
-  authDomain: "bar1985-d34ea.firebaseapp.com",
-  projectId: "bar1985-d34ea",
-  storageBucket: "bar1985-d34ea.firebasestorage.app",
-  messagingSenderId: "1010372504350",
-  appId: "1:1010372504350:web:5b7a4f88bb6a52d270b70f",
-  measurementId: "G-1TEEFS4FL2"
+  apiKey: "AIzaSyD0dfRMXPv35Qpe54P3xezjt4OAACM9Flc",
+  authDomain: "peluqueriambs.firebaseapp.com",
+  projectId: "peluqueriambs",
+  storageBucket: "peluqueriambs.appspot.com",
+  messagingSenderId: "67130614111",
+  appId: "1:67130614111:web:3e84f8b2e352b9fa59c6b4"
 };
 
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
-try { getAnalytics(app); } catch(e){ /* ignore if not available */ }
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db, deleteDoc, updateDoc, doc, signInWithEmailAndPassword, onAuthStateChanged, signOut, collection, addDoc, serverTimestamp, query, orderBy, onSnapshot };
+export { app, auth, db, signInWithEmailAndPassword, signOut, onAuthStateChanged, collection, addDoc, deleteDoc, doc, getDocs, serverTimestamp };
